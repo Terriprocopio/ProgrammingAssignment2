faaa21ba7fa611116fb5fffcb2b8f826073da45b
Terridactile <terriprocopio@hotmail.com>
2017-06-07 15:03:15 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of this project is to write a matrix function that includes a sub-set to inverse the matrix.
+## Part two will cache the inverse of the matrix.
 
-## Write a short comment describing this function
+## Function creates a matrix and includes a sub-set to inverse the matrix.
 
+## Main Cache Matrix function
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
 
+## Sub-set to inverse the matrix in the main function
+        set <- function(matrix) {
+                x <<- matrix
+                i <<- NULL
+}
+        
+## Function to return the matrix stored in the main function
+        get <- function() x
+        
+## Store the value for the inverse in the main function        
+        setInverse <- function(inverse)
+                i <<- inverse
+        
+## Get the inverse matrix stored in the main function
+        getInverse <<- function() i
+        
+## list of functions in Cache Matrix
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
+        
 }
 
 
